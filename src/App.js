bd786198ec8193c4dc566d0cde28d4d920b17244
Nicolás Angarita Ortiz <nico.las0315@hotmail.com>
2022-10-22 09:13:41 -0500

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("Cargando...");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+      .then((response) => response.text())
+      .then((data) => setText(data));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
