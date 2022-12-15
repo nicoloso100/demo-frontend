d83fba9d0f3c7e846c8f2e34dadc906e66fd6dd2
Nicolás Angarita Ortiz <nico.las0315@hotmail.com>
2022-12-14 19:11:24 -0500

    change text

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{text}</p>
+        <p>Hola</p>
         <a
           className="App-link"
           href="https://reactjs.org"
